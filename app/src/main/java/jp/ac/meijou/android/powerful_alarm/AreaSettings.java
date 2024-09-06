88c420d127ec85b26fda36da6045aed7eb96b4a5
t-haruki122 m <231205122@ccmailg.meijo-u.ac.jp>
2024-09-06 14:12:10 +0900

    AreaSettings Changed

--- app/src/main/java/jp/ac/meijou/android/powerful_alarm/AreaSettings.java
+++ app/src/main/java/jp/ac/meijou/android/powerful_alarm/AreaSettings.java
@@ -8,7 +8,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
+
 public class AreaSettings extends AppCompatActivity {
+    private ActivityAreaSettingsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +23,8 @@ public class AreaSettings extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+
     }
 }
