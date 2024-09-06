a5b53fb82a9d1bc4664c9cf3f3c7b0cb9879f080
Seiya Deguchi <seiya@meijo-u.ac.jp>
2024-09-06 14:44:26 +0900

    deleted binding

--- app/src/main/java/jp/ac/meijou/android/powerful_alarm/AreaSettings.java
+++ app/src/main/java/jp/ac/meijou/android/powerful_alarm/AreaSettings.java
@@ -11,7 +11,6 @@ import androidx.core.view.WindowInsetsCompat;
 
 
 public class AreaSettings extends AppCompatActivity {
-    private ActivityAreaSettingsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
