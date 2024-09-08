dad71aa03342bdec94365767407470c0bdadff95
RyzYuuki <dragongod674@yahoo.ne.jp>
2024-09-08 23:10:08 +0900

    Custom Spinner Add 3 .xml and change a .xml

--- app/src/main/java/jp/ac/meijou/android/powerful_alarm/AlarmSettings.java
+++ app/src/main/java/jp/ac/meijou/android/powerful_alarm/AlarmSettings.java
@@ -3,6 +3,10 @@ package jp.ac.meijou.android.powerful_alarm;
 import android.app.Activity;
 import android.health.connect.datatypes.SexualActivityRecord;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +16,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 import jp.ac.meijou.android.powerful_alarm.databinding.ActivityAlarmSettingsBinding;
 
+
 public class AlarmSettings extends AppCompatActivity {
 
     private ActivityAlarmSettingsBinding binding;
@@ -20,11 +25,34 @@ public class AlarmSettings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_alarm_settings);
+
+        binding = ActivityAlarmSettingsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // date
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                R.layout.custom_spinner,
+                getResources().getStringArray(R.array.date)
+        );
+
+        adapter.setDropDownViewResource(R.layout.custom_spinner_dropdown);
+        binding.date.setAdapter(adapter);
+
+        // sound
+        ArrayAdapter<String> soundAdapter = new ArrayAdapter<>(
+                this,
+                R.layout.custom_spinner,
+                getResources().getStringArray(R.array.sound)
+        );
+
+        soundAdapter.setDropDownViewResource(R.layout.custom_spinner_dropdown);
+        binding.sound.setAdapter(soundAdapter);
     }
 }
